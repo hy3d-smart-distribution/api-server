4c095e576c95ca7084e2786e196e3d0b703de76d
Choi Seungeon <chou610@naver.com>
2018-08-18 21:41:30 +0900

    set up database connections for gallery

--- routes/gallery.js
+++ routes/gallery.js
@@ -11,18 +11,24 @@ let config = require('../config')[env];
 let upload = multer({ storage: multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, __dirname + '/../public/gallery/'+ req.params.userid + "/");
+        //cb(null, 'C:/Users/chou6/Desktop/storage/a/'+ req.params.userid + "/");
     },
     filename: function (req, file, cb) {
         console.log(file);
         cb(null, file.originalname);
     }
 }),});
+let connection = mysql.createConnection({
+    host: config.database.host,
+    port: config.database.port,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.dbname
+});
 router.post('/:userid',upload.single('img'), function(req, res, next) {
     mkdir('C:/Users/chou6/Desktop/storage/a', function (err) {
         if (err) console.error(err);
-    });
-    mkdir('C:/Users/chou6/Desktop/storage/a', function (err) {
-        if (err) console.error(err);
+
     });
     res.status(201).json({message: "done"});
 
