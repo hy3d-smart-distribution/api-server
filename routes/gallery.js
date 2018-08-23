dd004557b0234fc74a2e6298f4a61954a8cb0037
Choi Seungeon <chou610@naver.com>
2018-08-24 00:46:00 +0900

    added custom authentication logic

--- routes/gallery.js
+++ routes/gallery.js
@@ -117,6 +117,7 @@ router.post('/', function (req, res, next) {
                     let store = upload.single('img');
                     store(req, res, function (err) {
                         if (err) {
+                            //
                             return  res.status(500).json({message: "error occured"});;
                         }
                         return res.status(200).json({message: "success"});
