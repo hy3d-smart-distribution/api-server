a92c9d358536d5230dc400b7b0d0541f3c465735
Choi Seungeon <chou610@naver.com>
2018-09-03 22:31:08 +0900

    login design & functional improvement

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,33 @@
 window.addEventListener('load', function () {
     var companyList = document.querySelector('#companyList');
     var btn_login = document.querySelector('#do_login');
+    var input_password = document.querySelector('#input_pw');
+    var message = document.querySelector('#login .message');
+    var page_login = document.querySelector('#login');
+    var page_upload = document.querySelector('#upload');
+
 
     var token = "";
     var app = {
         token: token,
-        companyList: companyList
+        companyList: companyList,
+        input_password: input_password,
+        btn_login: btn_login,
+        message: message,
+        page_login: page_login,
+        page_upload: page_upload
     };
 
+    input_password.addEventListener('keypress',catchEnter(app));
     btn_login.addEventListener('click', doLogin(app));
 });
+function catchEnter(app) {
+    return function f(e) {
+        if(e.which===13){
+            app.btn_login.click();
+        }
+    }
+}
 function doLogin(app) {
     return function f(e) {
         var id = document.querySelector('#input_id').value;
@@ -25,8 +43,19 @@ function doLogin(app) {
 function getToken(app) {
     return function f(e) {
         var json  =  JSON.parse(this.responseText);
+        console.log(json);
         var result = json.result;
         var description = json.description;
+        if(result==='error'){
+            if(description==='invalid_password'){
+                app.message.innerText = '비밀번호가 일치하지 않습니다.';
+            }
+            else if(description==='invalid_username'){
+                app.message.innerText = '존재하지 않는 아이디입니다.';
+            }
+        }else{
+            app.page_login.style.visibility = 'hidden';
+        }
         if(json.token!==undefined){
             var token = json.token;
             app.token = token;
